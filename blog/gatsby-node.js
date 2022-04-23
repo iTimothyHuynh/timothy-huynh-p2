7161188bf4f08c3a3762d14b27f1d63b0fefca91
TimothyHuynh212 <skyborn225@gmail.com>
2022-04-22 23:00:23 -0500

    Implemented createPages API & blog post query

--- blog/gatsby-node.js
+++ blog/gatsby-node.js
@@ -1,9 +1,34 @@
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  })
-}
+const path = require(`path`);
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        query MyQuery {
+          allContentfulBlogPost {
+            edges {
+              node {
+                id
+                slug
+              }
+            }
+          }
+        }
+    `).then(result => {
+      if (result.errors) {
+      reject(result.errors);
+    }
+    result.data.allContentfulBlogPost.edges.forEach((edge) => {
+      createPage({
+        path: edge.node.slug,
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          slug: edge.node.slug
+        }
+      })
+    })
+    resolve();
+    })
+  });
+};
