67ca1466c3e675e382c46f918841a6ba2cf835ec
TimothyHuynh212 <skyborn225@gmail.com>
2022-04-23 02:01:46 -0500

    Converted Stylesheet to Styled Component

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -8,10 +8,17 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
 
 import Header from "./header"
-import "./layout.css"
+import GlobalStyle from "./GlobalStyle"
 
+const Content = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0 1.0875rem 1.45rem;
+  padding-top: 0;
+`
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -25,7 +32,9 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <GlobalStyle />
+      <Header siteTitle={data.site.siteMetadata.title || `Title`} />
+      <Content>
       <div
         style={{
           margin: `0 auto`,
@@ -44,6 +53,7 @@ const Layout = ({ children }) => {
           <a href="https://www.gatsbyjs.com">Gatsby</a>
         </footer>
       </div>
+      </Content>
     </>
   )
 }
