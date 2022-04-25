39e36ca1c8ca7967ed17a8fb997cc09845c5c8b5
TimothyHuynh212 <skyborn225@gmail.com>
2022-04-24 22:17:31 -0500

    Created & Used Heading1 Component

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import styled from "styled-components"
+import { H1 } from "../Heading"
 
 const Outer = styled.header`
   background: ${props => props.theme.header.backgroundColor};
@@ -14,9 +15,7 @@ const Inner = styled.div`
   padding: 1.45rem 1.0875rem;
 `
 
-const H1 = styled.h1`
-  margin: 0px;
-`
+
 
 const StyledLink = styled(Link)`
   color: white;
