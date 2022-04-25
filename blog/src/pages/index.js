355270cb2af871a5c93cf6b79358a02c970ac088
TimothyHuynh212 <skyborn225@gmail.com>
2022-04-25 00:35:31 -0500

    Restructured the Home page

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,32 +1,37 @@
 import * as React from "react"
 import { graphql, Link } from "gatsby"
 import { GatsbyImage } from "gatsby-plugin-image"
-
+import styled from "styled-components"
+import { Box, Card, Image, Heading } from "rebass"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { List, ListItem } from "../components/List"
 
+const Grid = styled(Box)`
+  box-sizing: border-box;
+  margin: 0px;
+  min-width: 0px;
+  display: grid;
+  gap: 100px;
+  grid-template-columns: repeat(auto-fit, minmax(128px, 1fr));
+`
+
 const IndexPage = ({ data }) => (
   <Layout>
     <Seo title="Home" />
-    <List width={[1, 1/2, 2/3]} p={2}>
+    <Grid>
       {
         data.allContentfulBlogPost.edges.map(edge => (
-          <ListItem p={3}>
-            <Link to={edge.node.slug} key={edge.node.id}>{edge.node.title}</Link>
-            <div>
-              <GatsbyImage
-                image={edge.node.heroImage.gatsbyImageData}
-                alt="test"
-                />
-            </div>
-            <div>
-              {edge.node.body.childMarkdownRemark.excerpt}
-            </div>
-          </ListItem>
+          <Card key={edge.node.id} width={256} p={3}>
+            <Link to={edge.node.slug}>
+              <GatsbyImage image={edge.node.heroImage.gatsbyImageData} alt="hero image" />
+            </Link>
+            <Heading>{edge.node.title}</Heading>
+            <div>{edge.node.body.childMarkdownRemark.excerpt}</div>
+          </Card>
         ))
       }
-    </List>
+    </Grid>
   </Layout>
 )
 
