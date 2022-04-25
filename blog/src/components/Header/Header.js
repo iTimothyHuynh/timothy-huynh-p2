30a1e3acc13caa759ea09d0b6f30415379eb1a92
TimothyHuynh212 <skyborn225@gmail.com>
2022-04-24 23:07:52 -0500

    Created BaseButton Component

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import styled from "styled-components"
-import { H1 } from "..Heading"
+import { H1 } from "../Heading"
 import { Section } from "../Section"
 
 const Outer = styled.header`
